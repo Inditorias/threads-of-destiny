aa81094a6b255f3bd68a56319d50e8c9c8621d97
Inditorias <jstuts18@gmail.com>
2023-07-28 14:24:59 -0700

    Initial commit

---
+++ src/main/java/inditorias/destiny/DestinyModClient.java
@@ -0,0 +1,10 @@
+package inditorias.destiny;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DestinyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
