93cfb89afa7846491c03de0ef0614fad463c4610
Inditorias <jstuts18@gmail.com>
2024-09-16 17:37:53 -0700

    Initial Commit

---
+++ src/main/java/net/inditorias/threadsofdestiny/ThreadsOfDestiny.java
@@ -0,0 +1,15 @@
+package net.inditorias.threadsofdestiny;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThreadsOfDestiny implements ModInitializer {
+	public static final String MOD_ID = "threads-of-destiny";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/inditorias/threadsofdestiny/ThreadsOfDestinyClient.java
@@ -0,0 +1,10 @@
+package net.inditorias.threadsofdestiny;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThreadsOfDestinyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
